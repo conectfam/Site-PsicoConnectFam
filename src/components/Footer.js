8bda1768191e1ba8b95866a7a969e231a8a81537
matheusmiguel-santos <149403481+conectfam@users.noreply.github.com>
2023-10-31 15:46:06 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 // Footer.js
 import React from 'react';
 import './Footer.css';
-import qualityFooter from '../images/quality-footer.png'; // Ajuste o caminho conforme necessário
+
 
 function Footer() {
   return (
     <footer>
       <div className="footer-bar-1"></div>
       <div className="footer-bar-2">
-        <img src={qualityFooter} alt="Connect Fam" className="footer-image"/>
+        
       </div>
       <div className="footer-bar-3">
         <p>Todos os direitos reservados | Desenvolvido pela equipe de desenvolvimento da Connect Fam Tecnologia | 2023 ©</p>
